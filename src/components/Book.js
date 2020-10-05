ff70bc5b4ed2c0fa1a09c84614fef72e5e2b1204
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-05 22:47:19 +0100

    session 4 homework completed

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,41 @@
 import React from 'react';
+// import Book from '../models/books.json'
 
 const Book = (props) => {
+
+    let {id, 
+        volumeInfo: {title, authors, description, imageLinks},
+        saleInfo} 
+        = props.book;
+   
+        let amount = () => {
+            if (saleInfo.retailPrice) {
+                let amount = saleInfo.retailPrice.amount;
+                return amount
+            }else {
+                let amount = 'No Amount Set'; 
+                return amount
+            }
+        };
+ 
+        let url = () => {
+        if (imageLinks) {
+            let url = imageLinks.smallThumbnail;
+            return url;
+        } else {
+            let url = 'image not available';
+            return url;
+        }
+        };
+        
+
     return (
         <div>
-            <h2>{props.book.volumeInfo.title}</h2>
+            <h2>{title}</h2>
+            <h3>{authors}</h3>
+            <img src= {url()} alt={id}/>
+            <p>{amount()}</p>
+            <p>{description}</p>
         </div>
     );
 }
