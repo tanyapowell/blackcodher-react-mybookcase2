de4cafb01838aa99cc213b5fed371e2af9110a45
Tanya Powell <hello@tanyapowell.co.uk>
2020-10-13 22:55:25 +0100

    Updates

---
+++ precache-manifest.3b77c43009c54f28687dd1fb7d6e117a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef0678884e1c2aea45ef3e730815e7d",
+    "url": "/blackcodher-react-mybookcase2/index.html"
+  },
+  {
+    "revision": "94f3eb7f4f6bacdbceae",
+    "url": "/blackcodher-react-mybookcase2/static/css/main.53024bc6.chunk.css"
+  },
+  {
+    "revision": "e97cc6704ee76587e662",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.37352be9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.37352be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f3eb7f4f6bacdbceae",
+    "url": "/blackcodher-react-mybookcase2/static/js/main.681533df.chunk.js"
+  },
+  {
+    "revision": "d24be0c08303f8cd9285",
+    "url": "/blackcodher-react-mybookcase2/static/js/runtime-main.01888d37.js"
+  }
+]);
